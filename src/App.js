d87a1e2ae7abab64baf1e2dd8c1a802f52ba888f
Tinapen <cristinapenaflor20@gmail.com>
2024-09-02 14:55:54 +0800

    02Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { AllRoutes } from "./routes/AllRoutes";
-import './App.css';
+import { Header, Footer } from "./components";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <AllRoutes />
+      <Footer />
     </div>
   );
 }
