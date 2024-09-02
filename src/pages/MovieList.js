30f61bc65d8e6541882b400fb2cf8403df1f8b41
Tinapen <cristinapenaflor20@gmail.com>
2024-09-02 11:10:55 +0800

    01Tailwind

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,9 @@
 export const MovieList = () => {
   return (
     <main>
-      <div>MovieList</div>
+      <div className="text-2xl bg-sky-200 p-10 border-8 border-blue-900 rounded-lg m-5">
+        MovieList
+      </div>
     </main>
   );
 };
